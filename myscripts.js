c56cbb17932c3e94669a6056eadaa93eca0febfe
Scurtu Claudiu <37246308+isoon5@users.noreply.github.com>
2020-03-23 17:12:23 +0200

    landing page fixed

--- myscripts.js
+++ myscripts.js
@@ -1,10 +1,18 @@
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-  var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementsByClassName("navbar").style.top = "0";
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var navbar = document.getElementsByClassName("navbar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  confirm("Press a button!");
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
   } else {
-    document.getElementsByClassName("navbar").style.top = "-50px";
+    navbar.classList.remove("sticky");
   }
-  prevScrollpos = currentScrollPos;
 } 
