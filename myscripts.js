6d24c8ca4c780aa0d22833875b30e5c9c39f20c6
Scurtu Claudiu <37246308+isoon5@users.noreply.github.com>
2020-04-22 15:45:18 +0300

    +section struct

--- myscripts.js
+++ myscripts.js
@@ -2,15 +2,15 @@
 window.onscroll = function() {myFunction()};
 
 // Get the navbar
-var navbar = document.getElementsByClassName("navbar");
+var navbar = document.getElementById("myNavbar");
 
 // Get the offset position of the navbar
 var sticky = navbar.offsetTop;
 
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
-  confirm("Press a button!");
-  if (window.pageYOffset >= sticky) {
+  
+  if (window.pageYOffset > sticky) {
     navbar.classList.add("sticky")
   } else {
     navbar.classList.remove("sticky");
